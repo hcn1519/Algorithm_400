e3349b3202813f00d0ef8ae27f2460f8f46f2d4c
Changnam Hong <hcn1519@gmail.com>
2018-01-03 00:05:41 +0900

    (Edit) Problem 18

--- BOJ/Dynamic Proramming/BOJ_11726.swift
+++ BOJ/Dynamic Proramming/BOJ_11726.swift
@@ -5,18 +5,19 @@ let n = Int(readLine()!)!
 func tile(n: Int) -> Int {
     var dp = Array(repeating: 0, count: n + 1)
 
-    dp[1] = 1
-    dp[2] = 2
+    dp[0] = 1
+    dp[1] = 2
+
 
     for i in dp.indices {
-        if i == 0 || i == 1 || i == 2 {
+        if i == 0 || i == 1 {
             continue
         }
 
-        dp[i] = dp[i-1] % 10007 + dp[i-2] % 10007
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] %= 10007
     }
-
-    return dp[n]
+    return dp[n-1]
 }
 
 print(tile(n: n))
