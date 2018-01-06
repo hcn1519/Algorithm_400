4bd32e141e758f47451b2436b4a41c4ec9e84c09
Changnam Hong <hcn1519@gmail.com>
2018-01-06 23:53:57 +0900

    Problem 27

---
+++ BOJ/Dynamic Proramming/BOJ_10844/BOJ_10844.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class BOJ_10844 {
+  public static void main(String[] args) {
+    Scanner scin = new Scanner(System.in);
+
+    int n = scin.nextInt();
+
+    System.out.println(stairN(n));
+  }
+
+  static long stairN(int n) {
+    long dp[][] = new long[n+1][10];
+    long mod = 1000000000;
+
+    for(int i = 0; i<=9; i++) {
+      dp[1][i] = 1;
+    }
+
+    for(int i=2; i<=n; i++) {
+      for(int k=0; k<10; k++) {
+
+        dp[i][k] = 0;
+
+        if (k-1 >=0) {
+          dp[i][k] += dp[i-1][k-1];
+        }
+
+        if (k+1 <= 9) {
+          dp[i][k] += dp[i-1][k+1];
+        }
+        dp[i][k] %= mod;
+      }
+    }
+
+    long ans = 0;
+    for(int i=0; i<=9; i++){
+        ans += dp[n][i];
+    }
+    return ans % mod;
+  }
+}
